cf1926fcd0a07abd9b08b34fe105d75005ebdf10
fatemehbadiei <fatemehbadiei1995@gmail.com>
2022-03-07 11:17:18 +0330

    exercise
    This is an exercise

---
+++ app.js
@@ -0,0 +1,61 @@
+//normal
+
+
+// function learn(){
+//   return "hello world"
+// }
+// console.log(learn());
+
+// let learn  = function (){
+//     return "hello"
+// }
+// console.log(learn());
+
+//arrow
+
+
+// let learn = () =>{
+//     return "hello fatemeh"
+// }
+// console.log(learn());
+
+// let learn = ()=> "hello hamid";
+// console.log(learn());
+
+// let learn = (site) => `hello ${site}`;
+// console.log(learn("world"));
+
+// let learn = () => ({name : "fatemeh"});
+// console.log(learn());
+
+// let learn = _ => "hallo";
+// console.log(learn());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
